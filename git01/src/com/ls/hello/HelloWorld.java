e87fe03edbc8b94b1667b48657eee14d5b812c55
第二杯半价 <1421560409@qq.com>
2020-09-15 14:12:00 +0800

    第二次提交项目到本地仓库

--- git01/src/com/ls/hello/HelloWorld.java
+++ git01/src/com/ls/hello/HelloWorld.java
@@ -4,7 +4,7 @@ import com.ls.utils.Student;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("你好！");
+        System.out.println("你好，你已经被我修改过了！");
         Student student = new Student();
         student.show();
     }
